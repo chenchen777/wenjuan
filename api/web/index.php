278b325d4120adc739e7cc8dc26aa580fa9fc334
lichen <q59902165@163.com>
2021-10-28 15:43:56 +0800

    解决跨域

--- api/web/index.php
+++ api/web/index.php
@@ -1,9 +1,4 @@
 <?php
-header('Content-Type: text/html;charset=utf-8');
-header('Access-Control-Allow-Origin:*'); // *代表允许任何网址请求
-header('Access-Control-Allow-Methods:POST,GET,OPTIONS,DELETE'); // 允许请求的类型
-header('Access-Control-Allow-Credentials: true'); // 设置是否允许发送 cookies
-header('Access-Control-Allow-Headers: Content-Type,Content-Length,Accept-Encoding,X-Requested-with, Origin'); // 设置允许自定义请求头的字段
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
