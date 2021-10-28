af5e5391a0df880ae1c5f2795df3b78bf7289248
lichen <q59902165@163.com>
2021-10-28 16:25:04 +0800

    解决跨域

--- api/controllers/InvestigationController.php
+++ api/controllers/InvestigationController.php
@@ -8,6 +8,9 @@
 namespace api\controllers;
 
 use common\models\Investigation;
+use yii\filters\auth\HttpBearerAuth;
+use yii\filters\Cors;
+use yii\filters\VerbFilter;
 use yii\web\Response;
 use yii\base\Controller;
 use yii\filters\ContentNegotiator;
@@ -26,19 +29,54 @@ class InvestigationController extends Controller{
 
     public function behaviors()
     {
-        return [
+//        return [
+//            'contentNegotiator' => [
+//                'class' => ContentNegotiator::className(),
+//                'formats' => [
+//                    'application/json' => Response::FORMAT_JSON,
+//                ],
+//            ]
+////            'cors' => [
+////                'Origin' => ['*'],
+////                'Access-Control-Request-Method' => ['GET', 'POST'],
+////                'Access-Control-Request-Headers'=>['*']
+////            ],
+//        ];
+        $behaviors = [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [],
+            ],
+            'corsFilter' => [
+                'class' => Cors::className(),
+                'cors' => [
+                    'Origin' => ['*'],
+                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                    'Access-Control-Request-Headers' => ['*'],
+                    'Access-Control-Allow-Origin' => ['*'],
+                    'Access-Control-Allow-Credentials' => true,
+                    'Access-Control-Max-Age' => 86400,
+                    'Access-Control-Expose-Headers' => [],
+                ],
+            ],
             'contentNegotiator' => [
                 'class' => ContentNegotiator::className(),
                 'formats' => [
                     'application/json' => Response::FORMAT_JSON,
                 ],
             ]
-//            'cors' => [
-//                'Origin' => ['*'],
-//                'Access-Control-Request-Method' => ['GET', 'POST'],
-//                'Access-Control-Request-Headers'=>['*']
-//            ],
         ];
+        if (Yii::$app->getRequest()->getMethod() !== 'OPTIONS') {
+            $behaviors['authenticator'] = [
+                'class' => HttpBearerAuth::className(),
+                'optional' => [
+                    'login',
+                    'signup'
+                ],
+            ];
+        }
+
+        return $behaviors;
     }
 
     public function actionAdd()
