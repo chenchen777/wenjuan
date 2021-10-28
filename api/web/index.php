cd68a951125e2d55c012175639d56da8d99e733f
lichen <q59902165@163.com>
2021-10-28 15:12:54 +0800

    解决跨域

--- api/web/index.php
+++ api/web/index.php
@@ -13,5 +13,5 @@ $config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../config/main.php'),
     require(__DIR__ . '/../config/main-local.php')
 );
-
+header("Access-Control-Allow-Origin: *");
 (new yii\web\Application($config))->run();
