91d7aaefe9f72eca94f4e19356c8535cfc213fe2
lichen <q59902165@163.com>
2021-10-28 14:28:23 +0800

    解决跨域

--- api/controllers/InvestigationController.php
+++ api/controllers/InvestigationController.php
@@ -47,12 +47,7 @@ class InvestigationController extends Controller{
 
     public function actionAdd()
     {
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Credentials: true");
-        header("Access-Control-Max-Age: 1000");
-        header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
-        header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
-        header("Content-Type: application/json; charset=UTF-8");
+        header("Access-Control-Allow-Origin: http://que.kuaizhongcao.com");
         $param = \Yii::$app->request->post();
         if (is_array($param['solve_problem'])){
             $param['solve_problem'] = implode(',', $param['solve_problem']);
